5db655424ee2496f0518d92096a0ed9035007e28
Ilhama Novruzova <inovruzova07@gmail.com>
2023-11-22 18:54:39 +0400

    handling errors generally

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// get all products
+let data = [];
+
 fetch('https://dummyjson.com/products')
-.then(res => res.json())
-.then(console.log);
+    .then(res => res.json())
+    .then((json) => {
+        data = json;
+        console.log(data);
+    }).catch(error => {
+        console.error('Error fetching data:', error);
+    });
